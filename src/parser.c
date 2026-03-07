eca7e52a500c13464cf4a01ca956de3fccc66e88
tensorigami <tensorigami@users.noreply.github.com>
2026-03-06 16:20:05 -0800

    regenerate with ABI 14

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 133
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 93
@@ -2264,7 +2264,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 25},
   [2] = {.lex_state = 3},
@@ -4303,7 +4303,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_catform(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -4321,13 +4320,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_catform(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "catform",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 0,
-    },
   };
   return &language;
 }
